56b1e70257a8a04cd310f5ff3e963121b53753fd
A-Muhammadali <110370469+A-Muhammadali@users.noreply.github.com>
2023-08-24 13:51:44 +0500

    Update arithmetic_op06.py

--- arithmetic_op06.py
+++ arithmetic_op06.py
@@ -1,7 +1,7 @@
 #Create a variable called 'number' and assign any value of int to it.
 numer=1894
 #Divide the number by 2 and assign number.
-x=numer/1
+x=numer//1
 #Find the fraction of the number and assign to a variable called 'answer'.
 answer=numer-x
 #Print the answer.
